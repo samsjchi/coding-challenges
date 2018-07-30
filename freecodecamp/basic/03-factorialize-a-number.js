741d0e990ca91831c21c7da129ca088ce3d45c4e
samsjchi <samuel.sj.chi@gmail.com>
2018-07-30 18:00:17 -0400

    Solve Factorialize a Number

--- freecodecamp/basic/03-factorialize-a-number.js
+++ freecodecamp/basic/03-factorialize-a-number.js
@@ -4,7 +4,8 @@
 /*
 Return the factorial of the provided integer.
 
-If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+If the integer is represented with the letter n, a factorial is the product of all positive integers
+less than or equal to n.
 
 Factorials are often represented with the shorthand notation n!
 
