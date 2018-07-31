1a1e43f05af4ed929c03b266af7078d96d81ba4e
samsjchi <samuel.sj.chi@gmail.com>
2018-07-31 14:28:36 -0400

    Solve Boo who

---
+++ freecodecamp/basic/10-boo-who.js
@@ -0,0 +1,14 @@
+// Challenge: Boo who
+// Difficulty: Basic
+
+/*
+Check if a value is classified as a boolean primitive. Return true or false.
+
+Boolean primitives are true and false.
+*/
+
+function booWho(bool) {
+  return typeof bool === 'boolean';
+}
+
+booWho(null);
