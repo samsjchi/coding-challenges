a155bbb9b2c429ee169017bf01e17b69796bf118
samsjchi <samuel.sj.chi@gmail.com>
2018-07-31 12:22:33 -0400

    Solve Confirm the Ending

---
+++ freecodecamp/basic/06-confirm-the-ending.js
@@ -0,0 +1,16 @@
+// Challenge: Confirm the Ending
+// Difficulty: Basic
+
+/*
+Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the
+purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+*/
+
+function confirmEnding(str, target) {
+  let startIndex = str.length - target.length;
+  return str.substring(startIndex) === target ? true : false;
+}
+
+confirmEnding('Bastian', 'n');
